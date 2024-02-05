74d3ef49c6b76225d8e4c42e612f8a0225116dd5
Miad jfr <miad,jafari@live.com>
2024-02-05 12:52:05 +0330

    soon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,21 @@
-const nav = document.querySelector('.mobile-nav'),
-navMenuBtn = document.querySelector('.nav-menu-btn'),
-navCloseBtn = document.querySelector('.nav-close-btn')
+const nav = document.querySelector(".mobile-nav"),
+    navMenuBtn = document.querySelector(".nav-menu-btn"),
+    navCloseBtn = document.querySelector(".nav-close-btn")
 
 
-const navToggleFunc = () => nav.classList.toggle('active');
-
-navMenuBtn.addEventListener('click',navToggleFunc);
-navCloseBtn.addEventListener('click',navToggleFunc);
-
+const navTogglefunc = () => nav.classList.toggle("active");
+navMenuBtn.addEventListener("click",navTogglefunc)
+navCloseBtn.addEventListener("click",navTogglefunc)
 
 
 const themeBtn = document.querySelectorAll(".theme-btn");
 
-for (let i = 0; i < themeBtn.length; i++) {
+for (let i = 0; i < 100; i++) {
   themeBtn[i].addEventListener("click", function () {
     document.body.classList.toggle("light-theme");
     document.body.classList.toggle("dark-theme");
 
-    for (let j = 0; j < themeBtn.length; j++) {
+    for (let j = 0; j < 100; j++) {
       themeBtn[j].classList.toggle("light");
       themeBtn[j].classList.toggle("dark");
     }
